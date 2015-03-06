662b7289535c1c972a4cbe9d4fdfe1f98ab8d3c3
mucahitinel <mucahit.inel@hotmail.com>
2015-03-06 23:40:44 +0200

    hizmetler be-heading

--- pages/hizmetler.php
+++ pages/hizmetler.php
@@ -3,10 +3,34 @@
 	<h3>Size en iyi hizmeti verebilmek için tüm uzmanlığımızı konuşturuyoruz.</h4>
 </div>
 <div class="hizmetler-contains">
-	<div class="hizmet">a</div>
-	<div class="hizmet">a</div>
-	<div class="hizmet mr0">a</div>
-	<div class="hizmet">a</div>
-	<div class="hizmet">a</div>
-	<div class="hizmet mr0">a</div>
+	<div class="hizmet">
+		<div class="hz-head h1">
+			<span>Boyutlu (Cephe) Rölöve</span>
+		</div>
+	</div>
+	<div class="hizmet">
+		<div class="hz-head h2">
+			<span>Haritacılık Hizmetleri</span>
+		</div>
+	</div>
+	<div class="hizmet mr0">
+		<div class="hz-head h1">
+			<span>Proje Uygulama ve Kontrol</span>
+		</div>
+	</div>
+	<div class="hizmet">
+		<div class="hz-head h2">
+			<span>Kontrol ve Aplikasyon</span>
+		</div>
+	</div>
+	<div class="hizmet">
+		<div class="hz-head h1">
+			<span>Mimari Rölöve</span>
+		</div>
+	</div>
+	<div class="hizmet mr0">
+		<div class="hz-head h2">
+			<span>İç Mekan Rölöve</span>
+		</div>
+	</div>
 </div>
