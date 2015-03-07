18878f200430e980a0c64c96895d5cad39079e2b
mucahitinel <mucahit.inel@hotmail.com>
2015-03-07 14:39:35 +0200

    hizmetler-finished

--- pages/hizmetler.php
+++ pages/hizmetler.php
@@ -7,30 +7,99 @@
 		<div class="hz-head h1">
 			<span>Boyutlu (Cephe) Rölöve</span>
 		</div>
+		<div class="hz-context">
+			<span>
+				Binanın dış cephesine bakan kolonlar,
+				kat betonları, duvarlar, pencereler her katta ölçülerek,
+				katlar arasındaki ilişkiyi de gösterir röleve hazırlanır.  
+
+				Ölçülen her cephe için bir referans hattı oluşturularak,
+				ölçülen tüm noktaların bu referansa göre
+				içe ya da dışa ne kadar kaçık oldukları gösterilir.
+			</span>
+		</div>
 	</div>
 	<div class="hizmet">
 		<div class="hz-head h2">
 			<span>Haritacılık Hizmetleri</span>
+		</div>
+		<div class="hz-context">
+			<span>
+				Halihazır Harita
+
+				Plankote - Rölöve
+
+				Hacim Hesaplama
+
+				İmar Planı Uygulamaları
+
+				Kamulaştırma Haritaları
+
+				GPS Ölçü ve Uygulamaları
+			</span>
 		</div>
 	</div>
 	<div class="hizmet mr0">
 		<div class="hz-head h1">
 			<span>Proje Uygulama ve Kontrol</span>
 		</div>
+		<div class="hz-context">
+			<span>
+				Yol Projeleri
+
+				Enerji Nakil Hatları
+
+				Haberleşme Hatları
+
+				Betonarme Projeleri
+
+				Çelik Projeleri
+
+				Altyapı Projeleri
+			</span>
+		</div>
 	</div>
 	<div class="hizmet">
 		<div class="hz-head h2">
 			<span>Kontrol ve Aplikasyon</span>
+		</div>
+		<div class="hz-context">
+			<span>
+				Aplikasyon, harita veya planların yapımı amacı ile arazide tesis edilmiş olan
+				poligon noktası vb. gibi sabit noktalardan yararlanılarak yapılır.
+				Aplikasyon, çoğunlukla hassasiyet gerektiren çalışmalardır.
+				
+			</span>
 		</div>
 	</div>
 	<div class="hizmet">
 		<div class="hz-head h1">
 			<span>Mimari Rölöve</span>
 		</div>
+		<div class="hz-context">
+			<span>
+				Eski Eser Rölöve, 
+
+				Rölöve Projeleri,      
+
+				Restorasyon Projeleri,      
+
+				Restitüsyon Projeleri,
+
+				
+				Eski Eser Röleve çalışmaları için Lazer Total Station kullanılmaktadır.
+			</span>
+		</div>
 	</div>
 	<div class="hizmet mr0">
 		<div class="hz-head h2">
 			<span>İç Mekan Rölöve</span>
+		</div>
+		<div class="hz-context">
+			<span>
+				Mevcut yapının lazer metre ve total station yardımıyla hassas bir şekilde ölçülüp,
+				 plan kesit ve görünüşlerinin çıkarılması işlemidir.  
+			</span>
 		</div>
 	</div>
 </div>
